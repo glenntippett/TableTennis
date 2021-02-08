b1bd074e84be0ea322db244574199c2f36398082
Glenn Tippett <glenn.tippett@gmail.com>
2021-02-08 21:51:55 +1100

    implemented flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,19 +4,30 @@
 // that code so it'll be compiled.
 
 
+import 'plugins/flatpickr';
 
-import flatpickr from "flatpickr";
-import 'flatpickr/dist/flatpickr.min.css';
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
-flatpickr('.datepicker', {
-
+document.addEventListener('turbolinks:load', () => {
+  flatpickr('.datepicker', {
+    altInput: true,
+    enableTime: true,
+    "disable": [
+      function(date) {
+          // return true to disable
+          return (date.getDay() === 0 || date.getDay() === 6);
+      }
+  ],
+  "locale": {
+      "firstDayOfWeek": 0 // start week on Sunday
+  }
+  });
 });
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
