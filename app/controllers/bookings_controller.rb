75d9bfd425e4687ca136e7b059da88bb053bb6a8
Glenn Tippett <glenn.tippett@gmail.com>
2021-02-08 13:51:25 +1100

    create new booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to root_path
+    else
+      render new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :start_date, :end_date)
+  end
 end
