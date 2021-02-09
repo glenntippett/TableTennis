0b33bbece455d0aca0d36b1b5ab828e95972a812
Glenn Tippett <glenn.tippett@gmail.com>
2021-02-09 15:05:17 +1100

    post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    # @game = Game.find(params[:game_id])
     @post = Post.new(post_params)
-    # @booking.game = @game
     @post.user = current_user
-    if @booking.save
+    if @post.save
       redirect_to root_path
     else
       render :new
